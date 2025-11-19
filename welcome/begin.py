91828c63fed4d9e88129250a20fcb74a20a1c98f
MagyeraND <d.nkubito1@alustudent.com>
2025-11-19 12:29:23 +0200

    created the welcome module

---
+++ welcome/begin.py
@@ -0,0 +1,17 @@
+def display_welcome_screen():
+    """Display welcome screen with ASCII art"""
+    
+    print("\n" * 2)  # Add some space at top
+    
+    print("_" * 50)
+    print("=" + " " * 48 + "=")
+    print("=" + " " * 10 + "WELCOME TO STARTUP CONNECT" + " " * 12 + "=")
+    print("=" + " " * 48 + "=")
+    print("=" + " " * 5 + "Connecting Rwandan Founders, Mentors &" + " " * 13 + "=")
+    print("=" + " " * 12 + "Investors Together" + " " * 18 + "=")
+    print("=" + " " * 48 + "=")
+    print("=" * 50)
+    
+    print("\n")
+    input("Press Enter to continue...")  # ← This pauses until user presses Enter
+    print("\n")
